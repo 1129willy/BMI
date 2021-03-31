357c680f35602d4bca38b7300894c4d0c83087d7
willy <1129willy@gmail.com>
2021-03-31 10:44:20 +0800

    initial commit

--- app/src/main/java/com/example/app_frist/MainActivity.kt
+++ app/src/main/java/com/example/app_frist/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.app_frist.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,12 +16,21 @@ class  MainActivity : AppCompatActivity(){
         super .onCreate(savedInstanceState)
         //binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        input_height
+        input_weight
     }
     fun bmi (view:View) {
-        val weight = weight.text.toString().toFloat()
-        val height = height.text.toString().toFloat()
+        val weight = input_weight.text.toString().toFloat()
+        val height = input_height.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI",bmi.toString())
+        Toast.makeText(this,bmi.toString(),Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+                .setMessage(bmi.toString())
+                .setTitle("your bmi")
+                .setPositiveButton("ok",null)
+                .setNeutralButton("cancel",null)
+                .show()
     }
 }
 
